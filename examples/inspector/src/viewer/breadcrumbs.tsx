b7484d037a430d85086dd0f4bf4f7fbe5f95564c
marshennikovaolga <marshennikova@gmail.com>
2025-02-28 16:36:11 +0100

    navigation

--- examples/inspector/src/viewer/breadcrumbs.tsx
+++ examples/inspector/src/viewer/breadcrumbs.tsx
@@ -14,12 +14,15 @@ export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
     <div className="z-20 relative bg-indigo-400/10 hover:bg-indigo-400/25 backdrop-blur-sm rounded-xl inline-flex px-2 py-1 whitespace-pre transition-all items-center space-x-1 min-h-10">
       <button
         onClick={() => onBreadcrumbClick(-1)}
-        className="flex items-center justify-center p-1 rounded-md"
+        className="flex items-center justify-center p-1 rounded-md hover:bg-indigo-500/20 transition-colors"
         aria-label="Go to home"
       >
         <img src="jazz-logo.png" alt="Jazz Logo" className="size-5" />
       </button>
       {path.map((page, index) => {
+        const isRoot = index === 0;
+        const isRootOnly = isRoot && path.length === 1;
+
         return (
           <span key={index} className="inline-block first:pl-1 last:pr-1">
             {index === 0 ? null : (
@@ -27,9 +30,13 @@ export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
             )}
             <button
               onClick={() => onBreadcrumbClick(index)}
-              className="text-indigo-700 font-medium"
+              className={`text-indigo-700 font-medium ${
+                !isRootOnly
+                  ? "hover:text-indigo-900 hover:bg-indigo-500/20 px-2 py-1 rounded-xl transition-colors"
+                  : ""
+              }`}
             >
-              {index === 0 ? page.name || "Root" : page.name}
+              {isRoot ? page.name || "Root" : page.name}
             </button>
           </span>
         );
